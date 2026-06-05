98bcc1831d38fb1bf965516f34c6dc6413e303a2
mackliu <macklun@gmail.com>
2026-06-05 13:48:00 +0800

    使用整理index.php的方法,整理admin.php

--- index.php
+++ index.php
@@ -9,15 +9,16 @@
 </head>
 
 <body>
+
 <div id="cover" style="display:none; ">
 	<div id="coverr">
-    	<a style="position:absolute; right:3px; top:4px; cursor:pointer; z-index:9999;" onclick="cl(&#39;#cover&#39;)">X</a>
+    	<a style="position:absolute; right:3px; top:4px; cursor:pointer; z-index:9999;" onclick="cl('cover')">X</a>
         <div id="cvr" style="position:absolute; width:99%; height:100%; margin:auto; z-index:9898;"></div>
     </div>
 </div>
 <iframe style="display:none;" name="back" id="back"></iframe>
 	<div id="main">
-    	<a title="" href="./home_files/home.htm"><div class="ti" style="background:url(&#39;use/&#39;); background-size:cover;"></div><!--標題--></a>
+    	<a title="" href="index.php"><div class="ti" style="background:url(&#39;use/&#39;); background-size:cover;"></div><!--標題--></a>
         	<div id="ms">
              	<div id="lf" style="float:left;">
             		<div id="menuput" class="dbor">
