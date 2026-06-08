73bb7ebf0d8659214e850a66492e1ab585c92e4a
mackliu <macklun@gmail.com>
2026-06-08 09:15:09 +0800

    建立新增標題圖片的後端api及資料表

--- include/title.php
+++ include/title.php
@@ -11,5 +11,8 @@
             <td><input type="text" name="text"></td>
         </tr>
     </table>
-    <div class="cent"><input type="submit" value="新增"><input type="reset" value="重置"></div>
-    </form>
+    <div class="cent">
+        <input type="submit" value="新增">
+        <input type="reset" value="重置">
+    </div>
+</form>
