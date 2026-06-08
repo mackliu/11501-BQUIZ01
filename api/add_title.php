7b633d616df3b8a2b2f30d2b40752f74b9de1a71
mackliu <macklun@gmail.com>
2026-06-08 10:08:37 +0800

    建立共用函式檔db.php

--- api/add_title.php
+++ api/add_title.php
@@ -4,7 +4,7 @@ include_once "db.php";
 
 if(!empty($_FILES['img']['tmp_name'])){
     move_uploaded_file($_FILES['img']['tmp_name'],"../upload/".$_FILES['img']['name']);
-    $_POST['text']
+    $_POST['text'];
     $_POST['img']=$_FILES['img']['name'];
     $_POST['sh']=0;
     $Title->save($_POST);
